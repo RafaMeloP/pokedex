bc1b5ae8e61d7d21abde953cff8457bd05ce2c5f
RafaelrMelo <rafaelrodmelo26@gmail.com>
2020-06-28 16:45:29 -0300

    Adicionado vizualização de pokemons pela região

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var button, search, url, modal,id;
+var button, search, url, modal,id,url2,div,botaoe,botaod,idp,reg="2",nome;
 url = "https://pokeapi.co/api/v2/pokemon/";
+url2="https://pokeapi.co/api/v2/pokedex/";
 function pesquisa() {
     fetch(url + search.value).then(res => res.json()).
     then(res => {
@@ -58,6 +59,55 @@ function mudaPokemon (direcao){
 function sairModal() {
     modal.setAttribute("class", "off");
 }
+function pesquisar(){
+    nome="";
+    div.innerHTML="";
+    fetch(url2 + reg + "/").then(res=>res.json()).
+    then(res=>{
+        res.pokemon_entries.map(item=>{
+            nome=item.pokemon_species.name;
+            fetch(url+nome).then(result=>result.json()).
+            then(result=>{
+                idp=""+item.entry_number+"";
+                div.innerHTML+=`
+                    <div class="elemento" id=${idp} onclick="mostraPokemon('${result.name}')">
+                        <img src="${result.sprites.front_default}" alt="${result.name}">
+                    </div>
+                `;
+                if(item.entry_number==1)
+                    document.getElementById("1").style.display="block";
+                idp="1";
+            });
+        });
+    });
+}
+function mostraPokemon(nome){
+    search.value=nome;
+    pesquisa()
+}
+function muda(dir){
+    document.getElementById(idp).style.display="none";
+    if(dir==1){
+        idp++;
+    }
+    else{
+        idp--;
+        if(idp<1){
+            idp=1;
+        }
+    }
+    try{
+        document.getElementById(idp).style.display="block";
+    }
+    catch{
+        idp=idp-2;
+        muda(1);
+    }
+}
+function regiao(regiao){
+    reg=regiao;
+    pesquisar();
+}
 function inicia() {
     button = document.querySelector("header div button");
     button.addEventListener("click", ()=>{
@@ -71,5 +121,15 @@ function inicia() {
         if (tecla == 13 && search.value != "")
             pesquisa();
     })
+    div=document.getElementById("cont");
+    botaoe=document.querySelector("#elementos .esquerda");
+    botaoe.addEventListener("click",()=>{
+        muda(-1);
+    });
+    botaod=document.querySelector("#elementos .direita");
+    botaod.addEventListener("click",()=>{
+        muda(1);
+    });
+    pesquisar();
 }
 window.addEventListener("load", inicia);
